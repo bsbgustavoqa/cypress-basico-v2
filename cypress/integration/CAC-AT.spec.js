a37764f53b3345c74ffa2bb6f9473de5b55589ab
bsbgustavoqa <bsbgustavoalves@gmail.com>
2024-10-30 10:40:08 -0300

    teste da explicação do jefferson

--- cypress/integration/CAC-AT.spec.js
+++ cypress/integration/CAC-AT.spec.js
@@ -4,6 +4,7 @@ describe("Central de Atendimento ao Cliente TAT", function(){
 
     it("Verifica o título da aplicação", function(){
         cy.visit('C:/Users/Foton/Cypress/Cypress/cypress-basico-v2/src/index.html')
+        
         cy.title().should("be.equal","Central de Atendimento ao Cliente TAT")
     })
 })
