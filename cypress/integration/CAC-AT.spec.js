c70b5a5e2025db082612a46672942ed98bbd8a33
bsbgustavoqa <bsbgustavoalves@gmail.com>
2024-10-31 14:13:38 -0300

    Aula de interação com campos

--- cypress/integration/CAC-AT.spec.js
+++ cypress/integration/CAC-AT.spec.js
@@ -1,10 +1,32 @@
 /// <reference types="Cypress" />
 
-describe("Central de Atendimento ao Cliente TAT", function(){
+describe("Central de Atendimento ao Cliente TAT", function() {
+    this.beforeEach(() => {
+        cy.visit('./src/index.html')
+    })
 
-    it("Verifica o título da aplicação", function(){
-        cy.visit('C:/Users/Foton/Cypress/Cypress/cypress-basico-v2/src/index.html')
-        
+    it("Verifica o título da aplicação", function(){       
         cy.title().should("be.equal","Central de Atendimento ao Cliente TAT")
+    })
+
+    it("Preenche os campos obrigatórios e envia o formulário", function(){
+        cy.get("[id=firstName]")
+          .type("Gustavo")
+
+        cy.get("[id=lastName]")
+          .type("Alves")
+
+        cy.get("[id=email]")
+          .type("bsb@gmail.com")  
+          
+        cy.get("[id=open-text-area]")
+          .type("Teste de inclusão.") 
+          
+        cy.get("button[type=submit]")
+          .click()
+          .wait(500) 
+          
+        cy.get(".success") 
+          .should("be.visible")  
     })
 })
